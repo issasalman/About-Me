7458c3bde621cbf50aab5f9935179a92a93ec1dc
issasalman <issasalmanhu1996@gmail.com>
2021-07-05 14:26:10 +0300

    fixed

---
+++ js/app.js
@@ -0,0 +1,66 @@
+'use strict'
+
+let userName=prompt('What is your name?');
+alert('welcome ' +userName + ' Nice to meet you ^^' );
+
+
+let name=prompt('Is my name Issa?');
+if (name=='yes'|| 'y') {
+    console.log( name+"  my name is Issa");
+    alert('true my name is Issa')
+    
+} else {
+    alert('You are wrong my name is Issa')
+    
+}
+
+
+
+let age=prompt('Is my age 25?');
+if (age=='yes'|| 'y') {
+    console.log(age+"   my age is 25");
+    alert('true my age is 25')
+    
+} else {
+    alert('You are wrong my age is 25')
+
+    
+}
+
+let major=prompt('Am i an Engineer?');
+if (major=='yes'|| 'y') {
+    console.log(major+" I am an Engineer");
+    alert('true I am an Engineer')
+    
+} else {
+    alert('You are wrong I am engineer')
+
+    
+    
+}
+
+
+
+let car=prompt('Is my favourite car BMW??');
+if (car=='yes'|| 'y') {
+    console.log(car+" I like BMW So much");
+    alert('true I like BMW So much')
+    
+} else {
+    alert('You are wrong I like BMW')
+
+    
+}
+
+let travel=prompt('Do I like to visst Dubai?');
+if (travel=='yes'|| 'y') {
+    console.log(travel+" I visited it last year");
+    alert('true I visited it last year')
+    
+} else {
+    alert('You are wrong I like to visit Dubai')
+    
+}
+
+
+alert('Bye Bye  ' +userName + ' Nice to meet you ^^' );
